b46ce0f49f84960e87ba9a756c03436766d1164a
Steve-Carlton <63474298+Steve-Carlton@users.noreply.github.com>
2020-12-04 14:53:52 -0800

    WIP append broken

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -1,7 +1,7 @@
 //Get data.json into index.html
 
 //Assign info from data.json a variable. Copy/paste data.json
-const userProfiles =
+const userProfileList =
   [
     {
       "name": "Steve Smith",
@@ -22,5 +22,23 @@ const userProfiles =
 //Parse info in data.json to JS
 // var userProfiles = JSON.parse(userProfilesJSON);
 
+// Select HTML element and assign it a variable
+const userProfileContainer = document.getElementsByClassName("template-hook");
 
-// let userProfileList = document.getElementsByClassName("template-hook");
+
+//run a forEach on userProfileList
+userProfileList.forEach(function(el) {
+  // console.log(el); <=====test forEach function <=======
+  //Create a HTML element
+  let userProfile = document.createElement('p')
+  userProfile.innerHTML = 'TEST'
+  // add code block w/ string interpolation to backticks
+
+
+  //Append profile to selected HTML element, userProfileContainer
+  // userProfileContainer.append(userProfile)
+})
+
+
+//^^ABOVE APPEND NEEDS TO BE IN THE forEach FUNCTION.
+// index.js:36 Uncaught TypeError: userProfileContainer.append is not a function
